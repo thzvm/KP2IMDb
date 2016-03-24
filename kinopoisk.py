4c6ab18d88c8d878af7fca5f71781ebf1fe3f18c
gutmann method <ikafka@me.com>
2016-03-24 19:49:32 +0300

    Update kinopoisk.py

--- kinopoisk.py
+++ kinopoisk.py
@@ -1,13 +1,23 @@
 from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-filename = 'kinopoisk.csv'
+mail =  'thzvme@gmail.com'
+password = 'Test123Test'
+auth_page = 'https://secure.imdb.com/oauth/m_login'
+auth_status = None
+
+
+filename = '1.csv'
 time_sleep = 1
 
 data = []
 with open(filename, 'r') as csvfile:
+
     for line in csvfile:
 
-        data_line = line.replace('\n', '').split(';')
+        data_line = line.replace('\n', '').split(',')
+        print(data_line[1])
 
         if len(data_line[1]) == 0:
             data_line[1] = data_line[0]
@@ -19,66 +29,65 @@ with open(filename, 'r') as csvfile:
                          'rating': data_line[3]}
             #data.append(temp_dict)
 
-print(len(data))
 
-for i in range(len(data)):
-    print(data[i])
+browserProfile = webdriver.FirefoxProfile()
+browserProfile.set_preference('permissions.default.stylesheet', 1)
+browserProfile.set_preference('permissions.default.image', 2)
+browserProfile.set_preference('dom.ipc.plugins.enabled.libflashplayer.so', 'false')
+browser = webdriver.Firefox(firefox_profile=browserProfile)
+browser.set_window_size(400, 600)
 
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+def authorization(page , mail, password):
 
-driver = webdriver.Firefox()
+        browser.get(page)
 
-auth = 'https://secure.imdb.com/oauth/m_login'
-#auth
+        try:
+            mail_form = browser.find_element_by_id("email")
+            mail_form.send_keys(mail)
 
-driver.get(auth)
-auth_done = None
+            pass_form = browser.find_element_by_id("password")
+            pass_form.send_keys(password)
 
-while auth_done != True:
-    try:
-        mail = 'thzvme@gmail.com'#input('MAIL:')
-        mail_form = driver.find_element_by_id("email")
-        mail_form.send_keys(mail)
+            captcha = input('CAPTCHA:')
+            captcha_form = browser.find_element_by_name('captcha_answer')
+            captcha_form.send_keys(captcha)
 
-        password = 'Test123Test'#input('PASS:')
-        pass_form = driver.find_element_by_id("password")
-        pass_form.send_keys(password)
+            login = browser.find_element_by_id('submit')
+            login.send_keys(Keys.ENTER)
 
-        captcha = input('CAPTCHA:')
-        captcha_form = driver.find_element_by_name('captcha_answer')
-        captcha_form.send_keys(captcha)
+            assert "IMDb" in browser.title
+            return True
+        except:
+            return False
 
-        login = driver.find_element_by_id('submit')
-        login.send_keys(Keys.ENTER)
+def ratingtoimdb():
+     
+if __name__ == '__main__':
 
-        assert "IMDb" in driver.title
-        auth_done = True
+    while auth_status != True:
+        auth_status = authorization(page=auth_page, mail=mail, password=password)
 
-    except:
-        auth_done = False
-
-for i in range(len(data)):
-    driver.get('http://m.imdb.com')
-    assert "IMDb" in driver.title
-    query = data[i]['en']
-    find = driver.find_element_by_name("q")
-    find.send_keys(query)
-    find.send_keys(Keys.RETURN)
-    assert "No results found." not in driver.page_source
-    try:
-        title = str(driver.find_element_by_xpath('//div[@class="title"]').text)
-        title = title.split(' (')
-        result = driver.find_element_by_link_text(title[0])
-        result.send_keys(Keys.ENTER)
-        assert "No results found." not in driver.page_source
-        select = driver.find_element_by_name('rating')
-        select.send_keys(int(data[i]['rating']))
-        select.click()
-        sleep(2)
-        select.click()
-        sleep(0)
-        sleep(time_sleep)
-    except Exception as e:
-        print('FAIL:', query)
-        sleep(time_sleep)
+    for i in range(len(data)):
+        browser.get('http://m.imdb.com')
+        assert "IMDb" in browser.title
+        query = data[i]['en']
+        find = browser.find_element_by_name("q")
+        find.send_keys(query)
+        find.send_keys(Keys.RETURN)
+        assert "No results found." not in browser.page_source
+        try:
+            title = str(browser.find_element_by_xpath('//div[@class="title"]').text)
+            title = title.split(' (')
+            result = browser.find_element_by_link_text(title[0])
+            result.send_keys(Keys.ENTER)
+            assert "No results found." not in browser.page_source
+            select = browser.find_element_by_name('rating')
+            select.send_keys(int(data[i]['rating']))
+            select.click()
+            sleep(2)
+            select.click()
+            sleep(0)
+            sleep(time_sleep)
+        except Exception as e:
+            print('FAIL:', query)
+            sleep(time_sleep)
