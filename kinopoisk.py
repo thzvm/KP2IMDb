420368fdd16359f2a2205c44837c0d838426e4f9
gutmann method <ikafka@me.com>
2016-03-25 14:22:35 +0300

    Update kinopoisk.py

--- kinopoisk.py
+++ kinopoisk.py
@@ -1,41 +1,60 @@
+import configparser
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-mail =  'thzvme@gmail.com'
-password = 'Test123Test'
-auth_page = 'https://secure.imdb.com/oauth/m_login'
-auth_status = None
+def readconfigfile(file = 'config.ini'):
 
+    global CSVFILE, TXTLOGS, TIME_OUT, TIME_SLEEP, CSS, IMG, FLV, IMDb_mail, IMDb_pass, IMDb_auth, IMDb_stat, browser
+    config = configparser.ConfigParser()
 
-filename = '1.csv'
-time_sleep = 1
+    try:
+        config.read(file)
 
-data = []
-with open(filename, 'r') as csvfile:
+        CSVFILE     = config.get('SETTINGS', 'FILE')
+        TXTLOGS     = config.get('SETTINGS', 'LOGS')
+        TIME_OUT    = config.get('SETTINGS', 'TIME_OUT')
+        TIME_SLEEP  = config.get('SETTINGS', 'TIME_SLEEP')
+        CSS         = config.get('BROWSER', 'CSS')
+        IMG         = config.get('BROWSER', 'IMG')
+        FLV         = config.get('BROWSER', 'FLV')
+        IMDb_mail   = config.get('IMDb', 'IMDb_mail')
+        IMDb_pass   = config.get('IMDb', 'IMDb_pass')
+        IMDb_auth   = config.get('IMDb', 'IMDb_auth')
+        IMDb_stat   = False
 
-    for line in csvfile:
+        #####################################################################################
+        browserProfile = webdriver.FirefoxProfile()                                         #
+        browserProfile.set_preference('permissions.default.stylesheet', 1)                  #
+        browserProfile.set_preference('permissions.default.image', 2)                       #
+        browserProfile.set_preference('dom.ipc.plugins.enabled.libflashplayer.so', 'false') #
+        browser = webdriver.Firefox(firefox_profile=browserProfile)                         #
+        browser.set_window_size(400, 600)                                                   #
+        #####################################################################################
 
-        data_line = line.replace('\n', '').split(',')
-        print(data_line[1])
+    except configparser.NoSectionError as e:
+        print(file, 'error:', e)
+    except configparser.NoOptionError as e:
+        print(file, 'error:', e)
 
-        if len(data_line[1]) == 0:
-            data_line[1] = data_line[0]
-            temp_dict = {'ru' : data_line[0], 'en' : data_line[1], 'year' : data_line[2][:4],
-                         'rating' : data_line[3]}
-            data.append(temp_dict)
-        else:
-            temp_dict = {'ru': data_line[0], 'en': data_line[1], 'year': data_line[2][:4],
-                         'rating': data_line[3]}
-            #data.append(temp_dict)
+def aggregatedata(file):
 
+    data = []
+    with open(file, 'r') as csvfile:
 
-browserProfile = webdriver.FirefoxProfile()
-browserProfile.set_preference('permissions.default.stylesheet', 1)
-browserProfile.set_preference('permissions.default.image', 2)
-browserProfile.set_preference('dom.ipc.plugins.enabled.libflashplayer.so', 'false')
-browser = webdriver.Firefox(firefox_profile=browserProfile)
-browser.set_window_size(400, 600)
+        for line in csvfile:
+
+            data_line = line.replace('\n', '').split(',')
+            if len(data_line[1]) == 0:
+                data_line[1] = data_line[0]
+                temp_dict = {'ru' : data_line[0], 'en' : data_line[1], 'year' : data_line[2][:4],
+                             'rating' : data_line[3]}
+                data.append(temp_dict)
+            else:
+                temp_dict = {'ru': data_line[0], 'en': data_line[1], 'year': data_line[2][:4],
+                             'rating': data_line[3]}
+                data.append(temp_dict)
+    return data
 
 def authorization(page , mail, password):
 
@@ -60,13 +79,7 @@ def authorization(page , mail, password):
         except:
             return False
 
-def ratingtoimdb():
-     
-if __name__ == '__main__':
-
-    while auth_status != True:
-        auth_status = authorization(page=auth_page, mail=mail, password=password)
-
+def ratingtoimdb(data):
     for i in range(len(data)):
         browser.get('http://m.imdb.com')
         assert "IMDb" in browser.title
@@ -91,3 +104,13 @@ if __name__ == '__main__':
         except Exception as e:
             print('FAIL:', query)
             sleep(time_sleep)
+
+if __name__ == '__main__':
+    try:
+        readconfigfile()
+        while IMDb_stat != True:
+            IMDb_stat = authorization(page=IMDb_auth, mail=IMDb_mail, password=IMDb_pass)
+        ratingtoimdb(data=aggregatedata(file=CSVFILE))
+    except Exception as e:
+        print(e)
+
